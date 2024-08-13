35b840511499742a8c0f23bd1008e072adb7333c
G_03 <130713943+G3programmer@users.noreply.github.com>
2024-08-12 21:55:50 -0300

    Update login.php
    fé em Deus

--- login.php
+++ login.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de login</title>
     <style>
-        body{
+        body {
             font-family: Arial, Helvetica, sans-serif;
             background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
         }
-        div{
+
+        form {
             background-color: rgba(0, 0, 0, 0.6);
             position: absolute;
             top: 50%;
             left: 50%;
-            transform: translate(-50%,-50%);
+            transform: translate(-50%, -50%);
             padding: 80px;
             border-radius: 15px;
             color: #fff;
         }
-        input{
+
+        input {
             padding: 15px;
             border: none;
             outline: none;
             font-size: 15px;
         }
-        .inputSubmit{
+
+        .inputSubmit {
             background-color: dodgerblue;
             border: none;
             padding: 15px;
@@ -34,19 +38,22 @@
             border-radius: 10px;
             color: white;
             font-size: 15px;
-            
+
         }
-        .inputSubmit:hover{
+
+        .inputSubmit:hover {
             background-color: deepskyblue;
             cursor: pointer;
         }
     </style>
 </head>
+
 <body>
     <a href="home.php">Voltar</a>
     <div>
-        <h1>Login</h1>
+
         <form action="testLogin.php" method="POST">
+            <h1>Login</h1>
             <input type="text" name="email" placeholder="Email">
             <br><br>
             <input type="password" name="senha" placeholder="Senha">
@@ -55,4 +62,5 @@
         </form>
     </div>
 </body>
+
 </html>
